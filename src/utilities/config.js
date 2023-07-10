cd5bb21e67529f5e261f3c978a60729223d95312
Raj Dangol <rd0077@uah.edu>
2023-07-10 14:05:55 -0500

    folder restructure changes

--- src/utilities/config.js
+++
@@ -1,5 +0,0 @@
-export const INTHUB_RSS_FEED_URL = 'https://inthub.copernicus.eu/news/WebRss20';
-export const USGS_RSS_FEED_URL = 'https://6af2xo7p46.execute-api.us-east-2.amazonaws.com/dev/rss/';
-export const ALARM_URL = 'https://6af2xo7p46.execute-api.us-east-2.amazonaws.com/dev/alarms/';
-export const METRIC_URL = 'https://6af2xo7p46.execute-api.us-east-2.amazonaws.com/dev/metrics/';
-export const HLS_TITLE = 'Harmonized Landsat and Sentinel-2'
