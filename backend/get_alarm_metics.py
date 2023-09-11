01c7b71cae5c2e9d1af121bccb1bce7b7e1367c2
Raj Dangol <rd0077@uah.edu>
2023-09-11 14:09:07 -0500

    fetched alarm status based on metrics calculation rather than alarms

--- backend/get_alarm_metics.py
+++ backend/get_alarm_metics.py
@@ -3,6 +3,22 @@ import boto3
 from datetime import datetime, timedelta
 import os
 
+def granules_status_determination(value, thresholds):
+    if value < thresholds[0]:
+        return "DANGER"
+    elif value < thresholds[1]:
+        return "ALERT"
+    else:
+        return "OK"
+        
+def error_status_determination(value, thresholds):
+    if value > thresholds[0]:
+        return "DANGER"
+    elif value > thresholds[1] and value < thresholds[0]:
+        return "ALERT"
+    else:
+        return "OK"
+        
 def lambda_handler(event, context):
     cloudwatch = boto3.client('cloudwatch', region_name=os.getenv("region_name"))
     period = 24
@@ -10,253 +26,119 @@ def lambda_handler(event, context):
     
     s30_state_machine_arn = os.getenv("s30_state_machine_arn", "")
     l30_state_machine_arn = os.getenv("l30_state_machine_arn", "")
-    no_new_laads_alarm_name = 'No new LAADS'
+    no_new_laads_alarm_name = os.getenv("no_new_laads_alarm_name", "")
     
-    response = cloudwatch.describe_alarms(AlarmNames=[no_new_laads_alarm_name])
-    no_new_laads_alarm = response['MetricAlarms'][0]
-
-    metric_data_query = [
-        {
-            'Id': 'm0',
+    metric_data_query = []
+    for metric_name, arn in [('ExecutionsSucceeded', s30_state_machine_arn),
+                             ('ExecutionsStarted', s30_state_machine_arn),
+                             ('ExecutionsFailed', s30_state_machine_arn),
+                             ('ExecutionsSucceeded', l30_state_machine_arn),
+                             ('ExecutionsStarted', l30_state_machine_arn),
+                             ('ExecutionsFailed', l30_state_machine_arn)]:
+        metric_data_query.append({
+            'Id': 'm'+str(len(metric_data_query)),
             'MetricStat': {
                 'Metric': {
                     'Namespace': 'AWS/States',
-                    'MetricName': 'ExecutionsSucceeded',
+                    'MetricName': metric_name,
                     'Dimensions': [
                         {
                             'Name': 'StateMachineArn',
-                            'Value': s30_state_machine_arn
+                            'Value': arn
                         }
                     ]
                 },
                 'Period': 2592000,
                 'Stat': 'Sum'
             }
-        },
-        {
-            'Id': 'm1',
-            'MetricStat': {
-                'Metric': {
-                    'Namespace': 'AWS/States',
-                    'MetricName': 'ExecutionsStarted',
-                    'Dimensions': [
-                        {
-                            'Name': 'StateMachineArn',
-                            'Value': s30_state_machine_arn
-                        }
-                    ]
-                },
-                'Period': 2592000,
-                'Stat': 'Sum'
-            }
-        },
-        {
-            'Id': 'm2',
-            'MetricStat': {
-                'Metric': {
-                    'Namespace': 'AWS/States',
-                    'MetricName': 'ExecutionsFailed',
-                    'Dimensions': [
-                        {
-                            'Name': 'StateMachineArn',
-                            'Value': s30_state_machine_arn
-                        }
-                    ]
-                },
-                'Period': 2592000,
-                'Stat': 'Sum'
-            }
-        },
-        {
-            'Id': 'm3',
-            'MetricStat': {
-                'Metric': {
-                    'Namespace': 'AWS/States',
-                    'MetricName': 'ExecutionsSucceeded',
-                    'Dimensions': [
-                        {
-                            'Name': 'StateMachineArn',
-                            'Value': l30_state_machine_arn
-                        }
-                    ]
-                },
-                'Period': 2592000,
-                'Stat': 'Sum'
-            }
-        },
-        {
-            'Id': 'm4',
-            'MetricStat': {
-                'Metric': {
-                    'Namespace': 'AWS/States',
-                    'MetricName': 'ExecutionsStarted',
-                    'Dimensions': [
-                        {
-                            'Name': 'StateMachineArn',
-                            'Value': l30_state_machine_arn
-                        }
-                    ]
-                },
-                'Period': 2592000,
-                'Stat': 'Sum'
-            }
-        },
-        {
-            'Id': 'm5',
-            'MetricStat': {
-                'Metric': {
-                    'Namespace': 'AWS/States',
-                    'MetricName': 'ExecutionsFailed',
-                    'Dimensions': [
-                        {
-                            'Name': 'StateMachineArn',
-                            'Value': l30_state_machine_arn
-                        }
-                    ]
-                },
-                'Period': 2592000,
-                'Stat': 'Sum'
-            }
-        }
-    ]
-    
-    
-    
+        })
+        
     end_time = datetime.now()
     start_time = end_time - timedelta(hours=period)
-    
+        
+    no_new_laads_alarm_response = cloudwatch.describe_alarms(AlarmNames=[no_new_laads_alarm_name])
+    no_new_laads_alarm = no_new_laads_alarm_response['MetricAlarms'][0]
     
     response = cloudwatch.get_metric_data(
         MetricDataQueries=metric_data_query,
         StartTime=start_time,
         EndTime=end_time
     )
-    
-    s30_granulues_produced = response["MetricDataResults"][0]["Values"][0]
-    s30_granulues_produced_timestamp = response["MetricDataResults"][0]["Timestamps"][0]
-    s30_granulues_started = response["MetricDataResults"][1]["Values"][0]
-    s30_granulues_failed = response["MetricDataResults"][2]["Values"][0]
-    l30_granulues_produced = response["MetricDataResults"][3]["Values"][0]
-    l30_granulues_produced_timestamp = response["MetricDataResults"][0]["Timestamps"][0]
-    l30_granulues_started = response["MetricDataResults"][4]["Values"][0]
-    l30_granulues_failed = response["MetricDataResults"][5]["Values"][0]
-    
-    
-    s30_granulues_produced_status = ""
-    l30_granulues_produced_status = ""
-    s30_nominal_failed_status = ""
-    l30_nominal_failed_status = ""
-        
-    if s30_granulues_produced < 5000:
-        print("red")
-        s30_granulues_produced_status = "DANGER"
-    elif l30_granulues_produced < 7000:
-        print("yellow")
-        s30_granulues_produced_status = "ALERT"
-    else:
-        print("green")
-        s30_granulues_produced_status = "OK"
+    metric_results = response["MetricDataResults"]
 
-    if l30_granulues_produced < 5000:
-        print("red")
-        l30_granulues_produced_status = "DANGER"
-    elif l30_granulues_produced < 7000:
-        print("yellow")
-        l30_granulues_produced_status = "ALERT"
-    else:
-        print("green")
-        l30_granulues_produced_status = "OK"
-
-    s30_nominal_failed = (s30_granulues_failed/s30_granulues_started)
+    s30_granulues_produced_status = granules_status_determination(metric_results[0]["Values"][0], [5000, 7000])
+    l30_granulues_produced_status = granules_status_determination(metric_results[3]["Values"][0], [5000, 7000])
     
-    if s30_nominal_failed > 0.2:
-        print("red")
-        s30_nominal_failed_status = "DANGER"
-    elif s30_nominal_failed > 0.1 and s30_nominal_failed < 0.2:
-        print("yellow")
-        s30_nominal_failed_status = "ALERT"
-    else:
-        print("green")
-        s30_nominal_failed_status = "OK"
+    s30_nominal_failed_status = error_status_determination(metric_results[2]["Values"][0] / metric_results[1]["Values"][0], [0.2, 0.1])
+    l30_nominal_failed_status = error_status_determination(metric_results[5]["Values"][0] / metric_results[4]["Values"][0], [0.2, 0.1])
     
-    l30_nominal_failed = (l30_granulues_failed/l30_granulues_started)
     
-    if l30_nominal_failed > 0.2:
-        print("l30 red")
-        l30_nominal_failed_status = "DANGER"
-    elif l30_nominal_failed > 0.1 and l30_nominal_failed < 0.2:
-        print("l30 yellow")
-        l30_nominal_failed_status = "ALERT"
-    else:
-        print("l30 green")
-        l30_nominal_failed_status = "OK"
+    no_new_laads_alarm_info = {
+        'Atmospheric Parameters Received': {
+            'state': no_new_laads_alarm['StateValue'],
+            'state_transitioned_timestamp': no_new_laads_alarm['StateTransitionedTimestamp'],
+            'state_updated_timestamp': no_new_laads_alarm['StateUpdatedTimestamp']
+        }
+    }
         
     s30_alarms_res = {
+        **no_new_laads_alarm_info,
         'Produced Granules w/in Expected Range': {
             'state': s30_granulues_produced_status,
-            'state_transitioned_timestamp': s30_granulues_produced_timestamp,
-            'state_updated_timestamp': s30_granulues_produced_timestamp,
-        },
-        'Atmospheric Parameters Received': {
-            'state': no_new_laads_alarm['StateValue'],
-            'state_transitioned_timestamp': no_new_laads_alarm['StateTransitionedTimestamp'],
-            'state_updated_timestamp': no_new_laads_alarm['StateUpdatedTimestamp']
+            'state_transitioned_timestamp': metric_results[0]["Timestamps"][0],
+            'state_updated_timestamp': metric_results[0]["Timestamps"][0],
         },
         'Nominal % Processing Errors': {
             'state': s30_nominal_failed_status,
-            'state_transitioned_timestamp': s30_granulues_produced_timestamp,
-            'state_updated_timestamp': s30_granulues_produced_timestamp
+            'state_transitioned_timestamp': metric_results[0]["Timestamps"][0],
+            'state_updated_timestamp': metric_results[0]["Timestamps"][0]
         }
     }
-    
+
     l30_alarms_res = {
+        **no_new_laads_alarm_info,
         'Produced Granules w/in Expected Range': {
             'state': l30_granulues_produced_status,
-            'state_transitioned_timestamp': s30_granulues_produced_timestamp,
-            'state_updated_timestamp': s30_granulues_produced_timestamp,
-        },
-        'Atmospheric Parameters Received': {
-            'state': no_new_laads_alarm['StateValue'],
-            'state_transitioned_timestamp': no_new_laads_alarm['StateTransitionedTimestamp'],
-            'state_updated_timestamp': no_new_laads_alarm['StateUpdatedTimestamp']
+            'state_transitioned_timestamp': metric_results[3]["Timestamps"][0],
+            'state_updated_timestamp': metric_results[3]["Timestamps"][0],
         },
         'Nominal % Processing Errors': {
-            'state': s30_nominal_failed_status,
-            'state_transitioned_timestamp': s30_granulues_produced_timestamp,
-            'state_updated_timestamp': s30_granulues_produced_timestamp
+            'state': l30_nominal_failed_status,
+            'state_transitioned_timestamp': metric_results[3]["Timestamps"][0],
+            'state_updated_timestamp': metric_results[3]["Timestamps"][0]
         }
     }
     
-    if (
-        s30_nominal_failed_status == 'OK' and
-        no_new_laads_alarm['StateValue'] == 'OK'
-        ):
-            if s30_granulues_produced_status == 'OK':
-                s30_status = "OK"
-            else:
-                s30_status = 'ALERT'
-    else:
-        s30_status = 'DANGER'
+    s30_status = "DANGER" if s30_granulues_produced_status == "DANGER" or no_new_laads_alarm['StateValue'] == "DANGER" or s30_nominal_failed_status == "DANGER" else s30_granulues_produced_status
+    l30_status = "DANGER" if l30_granulues_produced_status == "DANGER" or no_new_laads_alarm['StateValue'] == "DANGER" or l30_nominal_failed_status == "DANGER" else l30_granulues_produced_status
 
-    if (
-        l30_nominal_failed_status == 'OK' and
-        no_new_laads_alarm['StateValue'] == 'OK'
-        ):
-            if l30_granulues_produced_status == 'OK':
-                l30_status = "OK"
-            else:
-                l30_status = 'ALERT'
-    else:
-        l30_status = 'DANGER'
+    # if (
+    #     s30_nominal_failed_status == 'OK' and
+    #     no_new_laads_alarm['StateValue'] == 'OK'
+    #     ):
+    #         if s30_granulues_produced_status == 'OK':
+    #             s30_status = "OK"
+    #         else:
+    #             s30_status = 'ALERT'
+    # else:
+    #     s30_status = 'DANGER'
+
+    # if (
+    #     l30_nominal_failed_status == 'OK' and
+    #     no_new_laads_alarm['StateValue'] == 'OK'
+    #     ):
+    #         if l30_granulues_produced_status == 'OK':
+    #             l30_status = "OK"
+    #         else:
+    #             l30_status = 'ALERT'
+    # else:
+    #     l30_status = 'DANGER'
     
-    alarms_res = []
-    alarms_res.append(
-        {'alarms': l30_alarms_res, 'status': l30_status, 'alarm_name': 'L30 Status', 'state_updated_timestamp': l30_granulues_produced_timestamp},
-    )
+    alarms_res = [
+        {'alarms': l30_alarms_res, 'status': l30_status, 'alarm_name': 'L30 Status', 'state_updated_timestamp': metric_results[3]["Timestamps"][0]},
+        {'alarms': s30_alarms_res, 'status': s30_status, 'alarm_name': 'S30 Status', 'state_updated_timestamp': metric_results[0]["Timestamps"][0]}
+    ]
     
-    alarms_res.append(
-        {'alarms': s30_alarms_res, 'status': s30_status, 'alarm_name': 'S30 Status', 'state_updated_timestamp': s30_granulues_produced_timestamp}
-    )
     return {
         'statusCode': 200,
         'body': json.dumps(alarms_res, default=str)
